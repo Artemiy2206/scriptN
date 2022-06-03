9a8a5de86fa8a81fb012782693fff35170eec2c7
Artemiy2206 <101861600+Artemiy2206@users.noreply.github.com>
2022-06-03 18:32:31 +0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 4 ,5, 1, 4, 5];
+let lastIndex = -1;
+const numWeSearch = 5;
+
+for (let i = 0; i <= numbers.length - 1; i++) 
+{
+ if (numbers[i] === numWeSearch)
+ {
+   lastIndex = i;
+ }
+}
+
+alert(lastIndex)
